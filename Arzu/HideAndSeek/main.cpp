b762533eca3ec7b91f24ce2f7ca5dd176a6817f7
Arzu Nisa Yalcınkaya <arzunisaylcnky@gmail.com>
2023-05-24 16:22:35 +0300

    Add ghost catch situation

--- Arzu/HideAndSeek/main.cpp
+++ Arzu/HideAndSeek/main.cpp
@@ -12,26 +12,32 @@ struct Player {
     int width, height; // Oyuncunun boyutu
     int speed; // Oyuncunun hızı
     int score; // Oyuncunun skoru
+    int whiteSqureSize = height * 4;
+    int whiteSlide = whiteSqureSize/2.0;
 };
 
 struct Ghost {
     int x, y; // Hayaletin konumu
     int width, height; // Hayaletin boyutu
     bool isHidden; // Hayaletin görünürlüğü
+    bool isCatch;
 };
 
 class GameWidget : public QWidget {
+    int scWidth = 1920;
+    int scHeight = 1080;
 public:
     GameWidget(QWidget *parent = nullptr)
-        : QWidget(parent), player({ 0, 50, 50, 50, 20, 0 }), player2({ 700, 50, 50, 50, 20, 0 }), ghosts(), timer()
+        : QWidget(parent), player({ 0, 50, 20, 20, 38, 0}), player2({ scWidth - 100, 50, 20, 20, 38, 0}), ghosts(), timer()
     {
-        setFixedSize(800, 600);
+
+        setFixedSize(scWidth, scHeight);
 
         // Rastgele hayaletler oluşturma
         for (int i = 0; i < numGhosts; ++i) {
             int x = QRandomGenerator::global()->bounded(width() - player.width);
             int y = QRandomGenerator::global()->bounded(height() - player.height);
-            ghosts.push_back({ x, y, 30, 30, true });
+            ghosts.push_back({ x, y, 20, 20, false, false});
         }
 
         // Timer'ı başlatma
@@ -49,28 +55,38 @@ protected:
         painter.setBrush(Qt::black);
         painter.drawRect(0, 0, width(), height());
 
+        // Beyaz kareyi çizme p1
+        painter.fillRect(player.x - player.whiteSlide, player.y - player.whiteSlide, player.width + player.whiteSqureSize, player.height + player.whiteSqureSize, Qt::gray);
+        // Oyuncuyu çizme p1
+        painter.fillRect(player.x, player.y, player.width, player.height, Qt::blue);
+
+        // Beyaz kareyi çizme p2
+        painter.fillRect(player2.x - player2.whiteSlide, player2.y - player2.whiteSlide, player2.width + player2.whiteSqureSize, player2.height + player2.whiteSqureSize, Qt::gray);
+        // Oyuncuyu çizme p2
+        painter.fillRect(player2.x, player2.y, player2.width, player2.height, Qt::green);
+
 
         // Hayaletleri çizme
         for (const auto& ghost : ghosts) {
             if (!ghost.isHidden) {
-                painter.fillRect(ghost.x, ghost.y, ghost.width, ghost.height, Qt::red);
+                painter.fillRect(ghost.x, ghost.y,ghost.width, ghost.height, Qt::red);
+            }
+            if (ghost.isCatch) {
+                painter.setFont(QFont("Arial", 15));
+                painter.drawText(ghost.x + 4, ghost.y + ghost.height - 2, "X");
             }
         }
 
-        // Oyuncuyu çizme p1
-        painter.fillRect(player.x, player.y, player.width, player.height, Qt::blue);
-        // Oyuncuyu çizme p2
-        painter.fillRect(player2.x, player2.y, player2.width, player2.height, Qt::green);
 
         // Skoru yazdırma p1
         painter.setPen(Qt::blue);
-        painter.setFont(QFont("Arial", 12));
-        painter.drawText(10, 20, "Score: " + QString::number(player.score));
+        painter.setFont(QFont("Arial", 28));
+        painter.drawText(10, 50, "Score: " + QString::number(player.score));
 
         // Skoru yazdırma p2
         painter.setPen(Qt::green);
-        painter.setFont(QFont("Arial", 12));
-        painter.drawText(700, 20, "Score: " + QString::number(player2.score));
+        painter.setFont(QFont("Arial", 28));
+        painter.drawText(scWidth - 200, 50, "Score: " + QString::number(player2.score));
     }
 
     void keyPressEvent(QKeyEvent *event) override {
@@ -110,24 +126,42 @@ protected:
     void timerEvent(QTimerEvent *event) {
         // Timer her tetiklendiğinde hayaletleri kontrol etme
         for (auto& ghost : ghosts) {
+            // Kareyle çarpışma kontrolü
+            if (ghost.isHidden && player.x - player.whiteSlide < ghost.x + ghost.width &&
+                player.x - player.whiteSlide + player.whiteSqureSize > ghost.x &&
+                player.y - player.whiteSlide < ghost.y + ghost.height &&
+                player.y - player.height  + player.whiteSqureSize> ghost.y)
+            {
+                ghost.isHidden = false;
+            }
             // Oyuncuyla çarpışma kontrolü
-            if (ghost.isHidden && player.x < ghost.x + ghost.width &&
+            if (!ghost.isCatch && player.x < ghost.x + ghost.width &&
                 player.x + player.width > ghost.x &&
                 player.y < ghost.y + ghost.height &&
                 player.y + player.height > ghost.y)
             {
-                ghost.isHidden = false;
                 player.score++;
+                ghost.isCatch = true;
                 qDebug() << "Player 1 scored! Score: " << player.score;
             }
 
-            if (ghost.isHidden && player2.x < ghost.x + ghost.width &&
+            // PLAYER 2
+            // Kareyle çarpışma kontrolü
+            if (ghost.isHidden && player2.x - player2.whiteSlide < ghost.x + ghost.width &&
+                player2.x - player2.whiteSlide + player2.whiteSqureSize > ghost.x &&
+                player2.y - player2.whiteSlide < ghost.y + ghost.height &&
+                player2.y - player2.height  + player2.whiteSqureSize> ghost.y)
+            {
+                ghost.isHidden = false;
+            }
+            // Oyuncuyla çarpışma kontrolü
+            if (!ghost.isCatch && player2.x < ghost.x + ghost.width &&
                 player2.x + player2.width > ghost.x &&
                 player2.y < ghost.y + ghost.height &&
                 player2.y + player2.height > ghost.y)
             {
-                ghost.isHidden = false;
                 player2.score++;
+                ghost.isCatch = true;
                 qDebug() << "Player 2 scored! Score: " << player2.score;
             }
         }
@@ -143,7 +177,6 @@ protected:
 
         if (gameFinished) {
             qDebug() << "Game finished! Final score: " << player.score;
-            timer.stop();
             // Oyun bittiğinde yapılacak işlemler
         }
 
@@ -152,7 +185,7 @@ protected:
     }
 
 private:
-    static constexpr int numGhosts = 10; // Hayalet sayısı
+    static constexpr int numGhosts = 20; // Hayalet sayısı
     Player player; // Oyuncu
     Player player2;
     std::vector<Ghost> ghosts; // Hayaletler
